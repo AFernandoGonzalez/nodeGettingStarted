a7504916b4b9cdcaacead06f9af7f95ad4519033
Fernando <ferg.developer@gmail.com>
2023-12-11 13:02:22 -0500

    desctructuting and rest operator

--- app.js
+++ app.js
@@ -35,3 +35,34 @@ const intervalId = setInterval(() => {
         clearInterval(intervalId);
     }
 } , 1000);
+
+
+
+//destructuring
+// const PI = Math.PI;
+// const E = Math.E;
+// const SQRT2 = Math.SQRT2;
+
+
+const {PI, E, SQRT2} = Math;
+
+
+//rest operator
+const [first, ...restOfItems] = [10, 20, 30, 40];
+
+const data = {
+    temp1: '001',
+    temp2: '002',
+    firstName: 'John',
+    lastName: 'Doe'
+}
+
+const {temp1, temp2, ...person} = data;
+
+
+const newArray = [...restOfItems];
+
+const newObject = {
+    ...person
+}
+
